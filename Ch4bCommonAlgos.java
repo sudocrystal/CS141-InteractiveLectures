0accfecebc4d06d21f90f9f4f7c9e2721701afc9
Crystal Hess <chess@shoreline.edu>
2025-01-04 15:30:52 -0800

    Create Ch4bCommonAlgos.java

---
+++ Ch4bCommonAlgos.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ch4bCommonAlgos {
+
+   public static void main(String[] args) {
+      
+      System.out.println( "Cumulative Sum = " + sumTo(10) );
+//       System.out.println( findMax(console, 4) + " was the highest value entered");
+//       System.out.println( replicateLetter("apple") );
+//       System.out.println( reverse("apple") );
+   }
+     
+   // This method should take a number as a paramenter
+   // and return all the numbers from 1 to n added together
+   // returns 1+2+3+...+n
+   public static int sumTo(int n) {
+      return 0;
+   }
+   
+   // This method should prompt for `n` int values
+   // The method should return the highest value entered
+   public static int findMax(Scanner console, int n) {
+      return 0;
+   }
+   
+   // This method should take a string as a parameter
+   // and return the string with each letter repeated
+   // replicate("apple") --> "aappppllee"
+   public static String replicateLetter(String s) {
+      return "";
+   }
+
+   // This method should take a string as a parameter
+   // and return the string reversed
+   // reverse("apple") --> "elppa"
+   public static String reverse(String s) {
+      return "";
+   }
+}

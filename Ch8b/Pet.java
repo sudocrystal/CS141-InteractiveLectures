f6d338b8ba2530a4e298e55253a224c6bff956c8
Crystal Hess <maybecrystal@yahoo.com>
2025-03-04 11:08:23 -0800

    Add files via upload

---
+++ Ch8b/Pet.java
@@ -0,0 +1,24 @@
+public class Pet {
+
+   // fields
+   private String name;
+   private String species;
+   private String primaryBreed;
+   private String secondaryBreed;
+
+   // constructor(s)
+   public Pet(String name, String species, String primaryBreed, String secondaryBreed) {
+      this.name = name;
+      this.species = species;
+      this.primaryBreed = primaryBreed;
+      this.secondaryBreed = secondaryBreed;
+   }
+
+   // accessors
+
+   // mutators
+
+   // toString
+
+
+}
